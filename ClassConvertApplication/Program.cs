ac671ffb17719063c8f97a63054a814ca410bb34
Shaddiecakes <78277625+Shaddiecakes@users.noreply.github.com>
2022-11-03 14:20:28 -0500

    Add files via upload

---
+++ ClassConvertApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassConvertApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Feed me some numbers!");
+            string feedInput = Console.ReadLine();
+            if (feedInput.Contains("."))
+            {
+                double doubleOutput = System.Convert.ToDouble(feedInput);
+                Console.WriteLine("I give you a double of " + doubleOutput);
+            }
+            else
+            {
+                int intOutput = System.Convert.ToInt32(feedInput);
+                Console.WriteLine("I give you an int of " + intOutput);
+            }
+        }
+    }
+}
